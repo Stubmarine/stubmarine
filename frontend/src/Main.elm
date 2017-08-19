92f1ac2bf6b38d03d92ecdf6b2fc6169800a8ced
Adam Neumann <noizwaves@users.noreply.github.com>
2017-08-19 16:02:56 -0600

    Simple app restyle with navbar and colours.

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,6 +1,6 @@
 import Json.Decode as Decode
 import Json.Decode.Extra exposing ((|:))
-import Html exposing (Html, div, text, dd, dt, dl, hr)
+import Html exposing (Html, div, text, dd, dt, dl, hr, span, ul, li)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 import Http
@@ -143,21 +143,36 @@ view model =
       Success email ->
         [ div [ class "email-detail" ]
           [ dl []
-            [ dt [] [ text "From" ]
+            [ dt [] [ text "From:" ]
             , dd [] [ text email.from ]
-            , dt [] [ text "To" ]
+            , dt [] [ text "To:" ]
             , dd [] [ text email.to ]
-            , dt [] [ text "Subject" ]
+            , dt [] [ text "Subject:" ]
             , dd [] [ text email.subject ]
             , hr [] []
-            , div [] [ text email.body ]
+            , div [ class "email-detail--body" ] [ text email.body ]
             ]
           ]
         ]
       Failure err ->
         [ div [ class "email-detail" ] [ text "Error" ] ]
   in
-    div [ class "email-pane" ] (emailList ++ emailDetail)
+
+    div []
+      [ div [ class "nav-bar" ]
+        [ div [ class "container container__nav" ]
+          [ div [ class "logo" ] [ text "Wallraff" ]
+          , ul [ class "nav" ]
+            [ li [ class "nav-item" ] [ text "Emails"]
+            ]
+          ]
+        ]
+      , div [ class "page-content" ]
+          [ div [ class "email-pane" ] (emailList ++ emailDetail)
+          ]
+      ]
+
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
